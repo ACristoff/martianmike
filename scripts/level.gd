fa29cd18d8ed80d62e9719e0406e04ae28b8f685
Alex Cristoffanini <acristoffanini@gmail.com>
2024-05-28 00:56:00 -0700

    timer implementation

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
+#song length 293
+@export var level_time = 5
 
 var player = null
+var timer_node = null
+var time_left = null
+var winCon = false
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
@@ -20,8 +25,24 @@ func _ready():
 	#print(traps)
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	time_left = level_time
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
 	pass # Replace with function body.
 
+#calling a signal every second, while negligible in this case seems like a bad way of doing this?
+#Wouldn't it be more efficient to create a timer and simply get the time left from there?
+func _on_level_timer_timeout():
+	if winCon == false:
+		time_left -= 1
+		#print(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -55,6 +76,7 @@ func _on_exit_body_entered(body):
 		if next_level != null:
 			exit.animate()
 			player.active = false
+			winCon = true
 			await get_tree().create_timer(1.5).timeout
 			get_tree().change_scene_to_packed(next_level)
 	pass
